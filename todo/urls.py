bdf99d8b5c4de5b73b5ea8509a095ae04254316f
Siddu <siddu124123@gmail.com>
2025-08-12 18:41:57 +0530

    completed mark_as_done functinality

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('add-task/', views.add_task, name='add_task')
+    path('add-task/', views.add_task, name='add_task'),
+    path('update-task/<int:id>/', views.update_task, name='update_task'),
+
 ]
 
--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,15 @@
 from django.http import HttpResponse
-from django.shortcuts import render,redirect
+from django.shortcuts import get_object_or_404, render,redirect
 from .models import Task
 # Create your views here.
 
 def add_task(request):
     task = request.POST['task']
     Task.objects.create(task=task)
+    return redirect('home')
+
+def update_task(request, id):
+    task = get_object_or_404(Task, pk=id)
+    task.is_completed = True
+    task.save()
     return redirect('home')
